25ff508352b59d4e807b778fa114cb48e193becd
fkhideaki <fkhideaki@gmail.com>
2023-06-07 08:54:36 +0900

    Update Mesh.py

--- Samples/Mesh.py
+++ Samples/Mesh.py
@@ -1,6 +1,13 @@
 import bpy
+import bmesh
 
 def removeAllVerts():
     bpy.ops.mesh.select_all(action='SELECT')
     bpy.ops.mesh.reveal()
     bpy.ops.mesh.delete(type='VERT')
+
+def getCurrentBMesh():
+    o = bpy.context.active_object
+    m = o.data
+    bm = bmesh.from_edit_mesh(m)
+    return bm
