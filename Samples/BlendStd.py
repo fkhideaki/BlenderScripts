bf6adb6df8b18710928490d1a6970534f90b5a39
fkhideaki <fkhideaki@gmail.com>
2023-06-12 17:33:28 +0900

    Add collection examples

--- Samples/BlendStd.py
+++ Samples/BlendStd.py
@@ -81,3 +81,13 @@ def setActiveObjectByName(name):
     if not obj:
         return
     setActiveObject(obj)
+
+def addNewCollection():
+    c = bpy.data.collections.new('')
+    bpy.context.scene.collection.children.link(c)
+    return c
+
+def moveObjectToCollection(collection, obj):
+    for c in obj.users_collection:
+        c.objects.unlink(obj)
+    collection.objects.link(obj)
