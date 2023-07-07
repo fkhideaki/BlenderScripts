1d2d6a0c4e5c5b98ae1712ab4ba24cb2f92f9790
fkhideaki <fkhideaki@gmail.com>
2023-07-07 09:53:56 +0900

    Update Collection.py

--- Samples/Collection.py
+++ Samples/Collection.py
@@ -6,6 +6,12 @@ def addNewCollection(name):
     bpy.context.scene.collection.children.link(c)
     return c
 
+def getOrCreateCollection(name):
+    for c in bpy.data.collections:
+        if c.name == name:
+            return c
+    return addNewCollection(name)
+
 def moveObjectToCollection(collection, obj):
     for c in obj.users_collection:
         c.objects.unlink(obj)
